919fcf4f023c3bfe8e81a0623fca1a52881ad064
Vancios86 <albuion86@gmail.com>
2021-06-30 17:49:27 +0100

    structure completed

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,3 +1,11 @@
+import {
+    addNewContact,
+    getContacts,
+    getContactWithID,
+    updateContact,
+    removeContact
+} from '../controllers/crmController';
+
 const routes = (app) => {
     app.route('/contact')
         .get((req, res, next) => {
@@ -5,20 +13,17 @@ const routes = (app) => {
             console.log(`Request from: ${req.originalUrl}`)
             console.log(`Request type: ${req.method}`)
             next();
-        }, (req, res, next) => {
-            res.send('GET request working')
-        })
+        }, getContacts)
 
-        .post((req, res) => {
-            res.send('POST request working')
-        });
+
+        .post(addNewContact);
 
     app.route('/contact/:contactID')
-        .put((req, res) =>
-            res.send('PUT request working'))
+        .get(getContactWithID)
 
-        .delete((req, res) =>
-            res.send('DELETE request working'))
+        .put(updateContact)
+
+        .delete(removeContact)
 }
 
 export default routes;
